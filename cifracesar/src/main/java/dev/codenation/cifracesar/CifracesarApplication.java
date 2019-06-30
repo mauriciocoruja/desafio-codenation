8569ba346ce00d63ee916ca22214cd00a361c3fd
mauriciocoruja <mbjr@outlook.com.br>
2019-06-30 13:14:17 -0300

    Refeito usando Spring

---
+++ cifracesar/src/main/java/dev/codenation/cifracesar/CifracesarApplication.java
@@ -0,0 +1,13 @@
+package dev.codenation.cifracesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifracesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifracesarApplication.class, args);
+	}
+
+}
